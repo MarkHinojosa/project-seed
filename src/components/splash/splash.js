749884bdec5eac2b2bd298c345369d7f67358a67
Mark Hinojosa <markhinojosa1212@gmail.com>
2018-03-10 01:22:13 -0600

    created splash using css grid

---
+++ src/components/splash/splash.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './splash.css';
+
+const Splash = () => {
+  return (
+    <div>
+      <div id="master" className="wrapper">
+        <section className="item1">
+          <h1>Welcome To Project-Seed</h1>
+          <p>A community driven, knowledge and experience sharing platform</p>
+          <p>Please Select Student's Age</p>
+          <div class="btnContainer">
+            <button value="infant" onClick={(e) => console.log(e.target.value)} class="btn">Infant</button>
+            <button value="toddler" onClick={(e) => console.log(e.target.value)} class="btn">Toddler</button>
+            <button value="child" onClick={(e) => console.log(e.target.value)} class="btn">Child</button>
+            <button value="adolescent" onClick={(e) => console.log(e.target.value)} class="btn">Adolescent</button>
+            <button value="adult" onClick={(e) => console.log(e.target.value)} class="btn">Adult</button>
+          </div>
+        </section>
+      </div >
+    </div >
+  )
+}
+
+export default Splash;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const state={
+  age:"blank"
+}
+
+ReactDOM.render(<App state={state}/>, document.getElementById('root'));
 registerServiceWorker();
